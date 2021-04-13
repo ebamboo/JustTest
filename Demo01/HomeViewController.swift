d55e7fd209b95b6dc317165f88a889031195bf42
yaoxu_home_iMac <1453810050@qq.com>
2021-04-13 21:34:29 +0800

    tableview顶部变化头

--- Demo01/HomeViewController.swift
+++ Demo01/HomeViewController.swift
@@ -7,32 +7,78 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
-    let nextBtn: UIButton = {
-        let btn = UIButton.init(frame: CGRect.init(x: 0, y: 0, width: 80, height: 44))
-        btn.setTitle("next", for: .normal)
-        btn.setTitleColor(.black, for: .normal)
-        btn.backgroundColor = UIColor.gray
-        btn.addTarget(self, action: #selector(nextAction(sender:)), for: .touchUpInside)
-        return btn
-    }()
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let tableView = UITableView()
+    let titleList = ["导航栏、状态栏设置", "Scroll View Header"]
+    
+    // MARK: - life cirle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Home"
         view.backgroundColor = UIColor.systemGroupedBackground
-        view.addSubview(nextBtn)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        nextBtn.center = view.center
+        let frame = CGRect.init(x: view.safeAreaInsets.left, y: view.safeAreaInsets.top, width: view.bounds.size.width - view.safeAreaInsets.left - view.safeAreaInsets.right, height: view.bounds.size.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom)
+        tableView.frame = frame
     }
     
-    @objc func nextAction(sender: Any) {
-        self.navigationController?.pushViewController(BarViewController(), animated: true)
+    // MARK: - table view
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleList.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = titleList[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            navigationController?.pushViewController(BarViewController(), animated: true)
+            return
+        }
+        if indexPath.row == 1 {
+            let homeSB = UIStoryboard.init(name: "HomeModule", bundle: nil)
+            let scrollViewHeaderVC = homeSB.instantiateViewController(identifier: "ScrollViewHeaderViewController")
+            navigationController?.pushViewController(scrollViewHeaderVC, animated: true)
+            return
+        }
+    }
+    
 }
---
+++ Demo01/Scroll View Header/ScrollViewHeaderViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ScrollViewHeaderViewController.swift
+//  Demo01
+//
+//  Created by 姚旭 on 2021/4/13.
+//
+
+import UIKit
+
+class ScrollViewHeaderViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    let titleList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+    
+    let tableHeaer: UIImageView = {
+        let image = UIImage.init(named: "scroll-view-header-1")
+        let view = UIImageView.init(image: image)
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let frame = CGRect.init(x: 0, y: 0, width: view.bounds.size.width, height: 200)
+        tableHeaer.frame = frame
+        print("\(frame)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        // 关键是给 tableview 添加一个和 ”头“ 等高的 ”头“
+        tableView.addSubview(tableHeaer)
+        tableView.tableHeaderView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
+    }
+
+    // MARK: - table view
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleList.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = titleList[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // MARK: - scroll view
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        if offsetY < 0 {
+            tableHeaer.frame = CGRect.init(x: 0, y: offsetY, width: scrollView.bounds.size.width, height: 200-offsetY)
+        }
+        
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+    }
+    
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+    }
+    
+}
